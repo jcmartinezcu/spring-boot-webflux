84d4cb3965a387c27ae9a1c6403b78538e99d092
jcmartinez <jcmartinezcu@gmail.com>
2020-06-11 10:48:34 +0200

    video 373

--- src/main/java/com/bolsadeideas/springboot/webflux/app/models/documents/Producto.java
+++ src/main/java/com/bolsadeideas/springboot/webflux/app/models/documents/Producto.java
@@ -12,10 +12,17 @@ public class Producto {
 	private String id;
 
 	private String nombre;
-	
+
 	private Double precio;
-	
+
 	private Date createAt;
+
+	public Producto() {}
+
+	public Producto(String nombre, Double precio) {
+		this.nombre = nombre;
+		this.precio = precio;
+	}
 
 	public String getId() {
 		return id;
